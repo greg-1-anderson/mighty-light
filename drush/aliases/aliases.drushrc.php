49bfbf0c57be5a430daf37a3f84861f668d3cca5
Greg Anderson <greg.1.anderson@greenknowe.org>
2016-02-27 09:56:10 -0800

    Add an example site-local alias file.

---
+++ drush/aliases/aliases.drushrc.php
@@ -0,0 +1,14 @@
+<?php
+
+$aliases["live"] = array (
+  'remote-host' => 'server.isp.com',
+  'remote-user' => 'www-admin',
+  'root' => '/srv/www/live.example.com',
+  'uri' => 'http://example.com',
+);
+$aliases["stage"] = array (
+  'remote-host' => 'server.isp.com',
+  'remote-user' => 'www-admin',
+  'root' => '/srv/www/stage.example.com',
+  'uri' => 'http://stage.example.com',
+);
